f8b3fe4f44cc1dffee8dec9c29fccd96e2e2ba18
Dimitry Besson <dbesson@pivotal.io>
2020-03-25 22:36:09 -0400

    Add print functions to structs for readability

--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
   "fmt"
-  "math"
 )
 
 // p initial
 // r interest rate
 // n compounding rate per time period
 // t num of time periods
-func getInterest(p float64, r float64, n, t int) float64 {
-  result := p * math.Pow((1 + r/12), 12 * 1)
-  return math.Round(result * 100) / 100
-}
+// func getInterest(p float64, r float64, n, t int) float64 {
+//   result := p * math.Pow((1 + r/12), 12 * 1)
+//   return math.Round(result * 100) / 100
+// }
 
 type World struct {
   players []*Player
@@ -22,10 +21,22 @@ func (w *World) addPlayer(p *Player) {
   w.players = append(w.players, p)
 }
 
+func (w *World) printCurrentState() {
+  fmt.Println("Current state:")
+
+  for _, p := range(w.players) {
+    p.print()
+  }
+}
+
 type Player struct {
   name string
   funds float64
   job *Job
+}
+
+func (p *Player) print() {
+  fmt.Printf("Player: %+v with Job: %+v\n", *p, *p.job)
 }
 
 func (p *Player) addJob(j *Job) {
@@ -38,14 +49,14 @@ type Job struct {
 }
 
 func main() {
-  fmt.Println("Hello!")
-  fmt.Println(getInterest(100, 0.02, 12, 1))
+  fmt.Println("Let the game begin!")
 
   w := World{}
-  p := &Player{name: "John", funds: 1000.00}
-  w.addPlayer(p)
-  fmt.Printf("%+v\n", w.players)
+  p := Player{name: "John", funds: 1000.00}
+  w.addPlayer(&p)
 
   j := Job{name: "teacher", salary: 50000.00}
   p.addJob(&j)
+
+  w.printCurrentState()
 }
